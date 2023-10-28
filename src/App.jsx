cfa56d5c2c538ee9ddd1731c48f563a97aac737a
sebastian motta <sebasmotta192002@gmail.com>
2023-10-28 16:27:45 -0500

    se agrego chakra ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../src/pages/home";
-import About from "../src/pages/About";
+import About from "../src/pages/Aboutus";
 import NotFound from "../src/pages/NotFound";
 import Menu from "../src/components/menu/Menu"
 import ListTasks from "../src/pages/ListTasks";
